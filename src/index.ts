b13d276f69637c88b9be7b5ffbe75ec40496ddda
mryhryki <mryhryki@gmail.com>
2024-01-10 08:16:59 +0900

    Fix code format

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export const convert = (_markdown: string): string => {
   // TODO
   return "<h1>H1</h1>";
-}
+};
--- src/test/heading.test.ts
+++ src/test/heading.test.ts
@@ -1,10 +1,10 @@
-import { assertEquals } from 'assert'
+import { assertEquals } from "assert";
 import { convert } from "../index.ts";
 
 Deno.test("Heading test", async (t) => {
   await t.step("Through", () => {
-    const markdown = "# H1"
-    const html = "<h1>H1</h1>"
-    assertEquals(convert(markdown), html)
-  })
-})
+    const markdown = "# H1";
+    const html = "<h1>H1</h1>";
+    assertEquals(convert(markdown), html);
+  });
+});
