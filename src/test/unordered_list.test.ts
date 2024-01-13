b57bd7f0684c24097d3d074e5db01ed9c3648244
mryhryki <mryhryki@gmail.com>
2024-01-13 21:49:50 +0900

    Update src/test/unordered_list.test.ts

--- src/test/unordered_list.test.ts
+++ src/test/unordered_list.test.ts
@@ -1,18 +1,45 @@
 import { assertEquals } from "assert";
 import { convert } from "../index.ts";
 
-Deno.test("Unordered-List test", () => {
-  const markdown = [
-    "- Item1", //
-    "- Item2",
-    "- Item3",
-  ].join("\n");
-  const html = [
-    "<ul>", //
-    "<li>Item1</li>",
-    "<li>Item2</li>",
-    "<li>Item3</li>",
-    "</ul>",
-  ].join("");
-  assertEquals(convert(markdown), html);
+Deno.test("Unordered-List test", async (t) => {
+  await t.step("Flat", () => {
+    const markdown = [
+      "- Item1", //
+      "- Item2",
+      "- Item3",
+    ].join("\n");
+    const html = [
+      "<ul>", //
+      "<li>Item1</li>",
+      "<li>Item2</li>",
+      "<li>Item3</li>",
+      "</ul>",
+    ].join("");
+    assertEquals(convert(markdown), html);
+  });
+
+  await t.step("Nested", () => {
+    const markdown = [
+      "- Item1", //
+      "- Item2",
+      "  - Item2-1",
+      "  - Item2-2",
+      "  - Item2-3",
+      "- Item3",
+    ].join("\n");
+    const html = [
+      "<ul>", //
+      "<li>Item1</li>",
+      "<li>Item2",
+      /**/ "<ul>",
+      /******/ "<li>Item2</li>",
+      /******/ "<li>Item2</li>",
+      /******/ "<li>Item2</li>",
+      /**/ "</ul>",
+      "</li>",
+      "<li>Item3</li>",
+      "</ul>",
+    ].join("");
+    assertEquals(convert(markdown), html);
+  });
 });
